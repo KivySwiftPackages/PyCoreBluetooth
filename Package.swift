949914453d2978a5947f3254deec9dfda67930f6
KivySwiftPackages <riper-chocks0g@icloud.com>
2023-10-12 19:46:59 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -12,14 +12,10 @@ let package = Package(
 
 	],
 	dependencies: [
-		.package(url: "https://github.com/PythonSwiftLink/KivySwiftLink", branch: "master"),
+        .package(url: "https://github.com/PythonSwiftLink/KivySwiftLink", from: .init(310, 0, 0)),
 		.package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", from: .init(0, 0, 0))
-		//.package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", branch: "main")
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
-	
 		.target(
 			name: "PyCoreBluetooth",
 			dependencies: [
