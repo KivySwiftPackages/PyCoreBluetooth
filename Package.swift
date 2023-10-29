11efd17cd15e32ba92179e8e20d776004490fbc3
KivySwiftPackages <147341125+KivySwiftPackages@users.noreply.github.com>
2023-10-29 17:30:30 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
 
 	],
 	dependencies: [
-        .package(url: "https://github.com/PythonSwiftLink/KivySwiftLink", from: .init(310, 0, 0)),
+        .package(url: "https://github.com/PythonSwiftLink/KivySwiftLink", from: .init(311, 0, 0)),
 		.package(url: "https://github.com/PythonSwiftLink/SwiftonizePlugin", from: .init(0, 0, 0))
 	],
 	targets: [
